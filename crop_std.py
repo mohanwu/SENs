9e04af252810e8faca6a1f79bf2ef6b675f15f61
Rees Simmons <rees.polylabs@gmail.com>
2017-03-01 20:56:54 -0500

    Added script to crop images to 120x120 images

--- crop_std.py
+++ crop_std.py
@@ -6,8 +6,14 @@ import math
 import json
 from PIL import Image
 
+#Will crop IM_NUM # of images from target folder
+target = "ALB"
+IM_NUM = 50
+# If IM_NUM is -1 then will do all images (except the last one)
+
+
 img_loc = os.getcwd()+'/train/'
-ALB = img_loc + 'ALB/' #replace alb with any other label to get the reuslts
+ALB = img_loc + target+'/'
 ALB_dict = dict()
 IM_NUM = 50
 
@@ -33,18 +39,18 @@ def resize(d=ALB_dict):
     #   if float(max(x_range,y_range)/min(x_range,y_range)) > 3 :
     #       continue
       translation = ((x_start + x_range)/2.0 - (x_start + big_range)/2.0, (y_start+y_range)/2.0 - (y_start + big_range)/2.0)
-      y_start = max(0, y_start + translation[1])
-      y_end = min(y_start + big_range, height)
-      x_start = max(0, x_start + translation[0])
-      x_end = min(x_start + big_range, width)
+      y_start = max(0, y_start + translation[1]-big_range*0.1)
+      y_end = min(y_start + big_range + big_range*0.1, height)
+      x_start = max(0, x_start + translation[0]-big_range*0.1)
+      x_end = min(x_start + big_range + big_range*0.1, width)
       crop_img = sample_img.crop((int(x_start),int(y_start),int(x_end),int(y_end)))
       #crop_img = sample_img[int(y_start - y_range):int(y_end + y_range), int(x_start - x_range):int(x_end + x_range)]
       crop_img = crop_img.resize((120,120), Image.ANTIALIAS)
-      crop_img.save(img_loc + 'cropped/ALB/' + filename, "JPEG")
-      print img_loc + 'cropped/ALB/' + filename
+      crop_img.save(img_loc + 'cropped/'+target+'/' + filename, "JPEG")
+      print img_loc + 'cropped/'+target+'/' + filename
 
 def crop():
-  with open('alb_labels_ht.json') as data_file:
+  with open(target.lower()+'_labels.json') as data_file:
     data = json.load(data_file)
     print type(data)
     for cur_entry in data[:IM_NUM]:
@@ -54,8 +60,11 @@ def crop():
       f_name = cur_entry['filename']
       # should just be two sets of points (top left + bottom right)
       for points in crop_info:
-        x_.append(points['x'])
-        y_.append(points['y'])
+        try:
+            x_.append(points['x'])
+            y_.append(points['y'])
+        except:
+            continue
       if len(y_) != 2 or len(x_) != 2:
         continue
       ALB_dict[f_name] = (x_, y_)
