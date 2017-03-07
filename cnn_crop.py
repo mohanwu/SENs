ffb7e957c7e501279a590395a95c3d4f17d66105
Rees Simmons <rees.polylabs@gmail.com>
2017-03-06 21:52:41 -0500

    Updated CNN and created testing set

--- cnn_crop.py
+++ cnn_crop.py
@@ -1,10 +1,10 @@
-'''Trains a simple convnet on the MNIST dataset.
-Gets to 99.25% test accuracy after 12 epochs
-(there is still a lot of margin for parameter tuning).
-16 seconds per epoch on a GRID K520 GPU.
+'''
+Mostly code from
+https://github.com/fchollet/keras/blob/master/examples/mnist_cnn.py
 '''
 
 from __future__ import print_function
+import os
 import numpy as np
 np.random.seed(1337)  # for reproducibility
 import cropped_images
@@ -18,7 +18,7 @@ from keras import backend as K
 
 batch_size = 128
 nb_classes = 6
-nb_epoch = 12
+nb_epoch = 25
 
 # input image dimensions
 img_rows, img_cols = 64, 64
@@ -31,22 +31,27 @@ kernel_size = (3, 3)
 
 # the data, shuffled and split between train and test sets
 
-X_train,y_train = cropped_images.load_data()
+X_train,y_train,X_test,y_test = cropped_images.load_data()
 # (X_train, y_train), (X_test, y_test) = mnist.load_data()
 if K.image_dim_ordering() == 'th':
     X_train = X_train.reshape(X_train.shape[0], 3, img_rows, img_cols)
+    X_test = X_test.reshape(X_test.shape[0], 3, img_rows, img_cols)
     input_shape = (3, img_rows, img_cols)
 else:
     X_train = X_train.reshape(X_train.shape[0], img_rows, img_cols, 3)
+    X_test = X_test.reshape(X_test.shape[0], img_rows, img_cols, 3)
     input_shape = (img_rows, img_cols, 3)
 
 X_train = X_train.astype('float32')
 X_train /= 255
+X_test = X_test.astype('float32')
+X_test /= 255
 print('X_train shape:', X_train.shape)
 print(X_train.shape[0], 'train samples')
 
 # convert class vectors to binary class matrices
 Y_train = np_utils.to_categorical(y_train.T, nb_classes)
+Y_test = np_utils.to_categorical(y_test.T, nb_classes)
 
 model = Sequential()
 
@@ -71,4 +76,11 @@ model.compile(loss='categorical_crossentropy',
               metrics=['accuracy'])
 
 model.fit(X_train, Y_train, batch_size=batch_size, nb_epoch=nb_epoch,
-          verbose=1)
+          verbose=1,validation_data=(X_test, Y_test))
+
+score = model.evaluate(X_test, Y_test, verbose=0)
+print('Test score:', score[0])
+print('Test accuracy:', score[1])
+
+model.save_weights("model_weights.h5")
+print("Saved model to disk")
--- cropped_images.py
+++ cropped_images.py
@@ -1,9 +1,9 @@
+import numpy as np
 def create_pickle():
     import os
-    import numpy as np
     from PIL import Image
     import pickle
-
+    np.random.seed(5)
     print "Creating Pickle"
     crop_folders = ['ALB', 'BET', 'DOL', 'LAG', 'SHARK', 'YFT']
     X = []
@@ -15,12 +15,22 @@ def create_pickle():
         Y += [label for x in xrange(len(temp_file_names))]
         X += temp_file_names
 
+    #Creates Images from filepaths
     X = map(lambda f_name: np.asarray( Image.open(f_name), dtype='uint8' ), X)
-    img_data['X'] = np.asarray(X,dtype='uint8')
-    img_data['Y'] = np.asarray(Y,dtype='uint8')
+
+    X = np.asarray(X,dtype='uint8')
+    Y = np.asarray(Y,dtype='uint8')
+    Y.shape = (1,Y.shape[0])
+
+    #Shuffles data
+    X_train,y_train,X_test,y_test = test_train_split(X,Y,0.7)
+
+    img_data['X_train'] = X_train
+    img_data['y_train'] = y_train
+    img_data['X_test'] = X_test
+    img_data['y_test'] = y_test
     with open('test_img_data.pkl', 'wb') as my_pickle:
         pickle.dump(img_data,my_pickle)
-
 
 def load_data():
     import os
@@ -29,4 +39,17 @@ def load_data():
     import pickle
     with open("test_img_data.pkl","rb") as img_f:
         data = pickle.load(img_f)
-    return (data['X'],data['Y'])
+    return (data['X_train'],data['y_train'],data['X_test'],data['y_test'])
+
+def test_train_split(X_data,y_data,percentage):
+    perm = np.random.permutation(np.arange(X_data.shape[0]))
+    X_data = X_data[perm,:]
+    y_data = y_data[:,perm]
+    train_size = int(X_data.shape[0]*percentage)
+
+    X_train = X_data[:train_size,:]
+    y_train = y_data[:,:train_size]
+    X_test = X_data[train_size:,:]
+    y_test = y_data[:,train_size:]
+
+    return (X_train,y_train,X_test,y_test)
